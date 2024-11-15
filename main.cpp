dab24054d3864a62ea435c5442089b089f64463d
Jed Aficial's PC <jedaficial1@gmail.com>
2024-11-14 23:04:21 -0800

    10-minute update

--- main.cpp
+++ main.cpp
@@ -7,11 +7,30 @@
 #include "Car-1.h"
 
 using namespace std;
+void deque_print(deque<Car> );
 
 int random_probability(){
     int random_prob;
     random_prob = (rand() % 100) + 1;
     return random_prob;
+}
+
+// deque_print function outputs the contents of the deque
+void deque_print(deque<Car> toll_booth){
+    if (toll_booth.empty()){
+        cout << "Queue:" << endl;
+        cout << setw(4) << "" << "Empty" << endl;
+        return;
+    }
+    int i = 0;
+    cout << "Queue:" << endl;
+    for (auto element : toll_booth){
+        Car temp_car = toll_booth[i];
+        cout << setw(4) << "";
+        toll_booth[i].print();
+        i++;
+    }
+    cout << endl;
 }
 
 int main(){
@@ -37,15 +56,28 @@ int main(){
     }
 
     int time_op = 1;
+    Car temp_car_paid;
     // run simulation until all cars have left the toll booth
     while(!toll_booth.empty()){
         int probability = random_probability();
-        cout << "Time: " << time_op << " Operation: Joined lane:"
+        
         if (probability <= 45){ // another car will join the line for the toll booth
             temp_car_obj = Car();
+            cout << "Time: " << time_op << " Operation: Joined lane:";
+            temp_car_obj.print();
             toll_booth.push_back(temp_car_obj);
+            time_op++;
+            deque_print(toll_booth);
+        }
 
-        } 
+        else if (probability > 45) {
+            temp_car_paid = toll_booth.front();
+            cout << "Time: " << time_op << " Operation: Car Paid: ";
+            temp_car_paid.print();
+            toll_booth.pop_front();
+            deque_print(toll_booth);
+            time_op++;
+        }
 
     }
     return 0;
