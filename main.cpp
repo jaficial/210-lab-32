9236236aaf70c9e6193bd396f1dcd19f3dd0c337
Jed Aficial's PC <jedaficial1@gmail.com>
2024-11-14 23:09:30 -0800

    10-minute update

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 void deque_print(deque<Car> );
 
+// random_probability function returns a random probability between 1 and 100
 int random_probability(){
     int random_prob;
     random_prob = (rand() % 100) + 1;
@@ -37,6 +38,7 @@ int main(){
     srand(time(0));
     deque<Car> toll_booth;
     Car temp_car_obj;
+    Car temp_car_paid;
 
     // start off with 2 cars in line:
     for (int i = 0; i < 2; i++){
@@ -44,8 +46,7 @@ int main(){
         toll_booth.push_back(temp_car_obj);
     }
 
-    // NOTE: Trying to figure out how to output the deque
-    // NOTE: When printing out loop: "toll_booth[i].print();" works
+    // NOTE: When printing out deque: "toll_booth[i].print();" works
     int i = 0;
     cout << "Initial queue:" << endl;
     for (auto element : toll_booth){
@@ -56,12 +57,11 @@ int main(){
     }
 
     int time_op = 1;
-    Car temp_car_paid;
     // run simulation until all cars have left the toll booth
     while(!toll_booth.empty()){
         int probability = random_probability();
         
-        if (probability <= 45){ // another car will join the line for the toll booth
+        if (probability <= 45){ // if probability is less than or equal to 45, a car joins the deque and is pushed to the back of the deque
             temp_car_obj = Car();
             cout << "Time: " << time_op << " Operation: Joined lane: ";
             temp_car_obj.print();
@@ -70,7 +70,7 @@ int main(){
             deque_print(toll_booth);
         }
 
-        else if (probability > 45) {
+        else if (probability > 45) { // if probability is greater than 45%, car at the front of the deque "pays" and front element is popped
             temp_car_paid = toll_booth.front();
             cout << "Time: " << time_op << " Operation: Car Paid: ";
             temp_car_paid.print();
